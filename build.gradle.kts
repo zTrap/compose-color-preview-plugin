84efbd726e6d701e5adc002096fdb509788b2ec9
Peter Gulko (zTrap) <ztrap.developer@gmail.com>
2024-04-10 19:56:41 +0300

    [Release] v0.0.3 [Features] - Added display of colored icon for using fields (chaining proxy fields is also supported) [Bug Fixes] - The property list now displays correctly when two or more colors are displayed on the same line

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,11 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.22"
 }
 
+val ideMinVersion = "241"
+val pluginMajorVersion = "0.0.3"
+
 group = "ru.ztrap.plugin.idea"
-version = "0.0.2-241"
+version = "$pluginMajorVersion-$ideMinVersion"
 
 repositories {
     mavenCentral()
@@ -16,8 +19,8 @@ repositories {
 
 // See https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2024.1")
-    type.set("IC")
+    version = "2024.1"
+    type = "IC"
 
     plugins.set(
         listOf(
@@ -45,18 +48,18 @@ tasks {
     }
 
     patchPluginXml {
-        version.set(project.version.toString())
-        sinceBuild.set("241")
-        untilBuild.set("251.*")
+        version = project.version.toString()
+        sinceBuild = ideMinVersion
+        untilBuild = "251.*"
     }
 
     signPlugin {
-        certificateChainFile.set(file(prop("publishing.plugin.key.chain")))
-        privateKeyFile.set(file(prop("publishing.plugin.key")))
-        password.set(prop("publishing.plugin.password"))
+        certificateChainFile = file(prop("publishing.plugin.key.chain"))
+        privateKeyFile = file(prop("publishing.plugin.key"))
+        password = prop("publishing.plugin.password")
     }
 
     publishPlugin {
-        token.set(prop("publishing.plugin.token"))
+        token = prop("publishing.plugin.token")
     }
 }
